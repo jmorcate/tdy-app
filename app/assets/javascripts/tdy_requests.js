386a5b7e95cd9477a0bcf2889706899edc9581e2
Joaquin Morcate <jmorcate@gmail.com>
2017-05-03 14:37:50 +0000

    Moved the javascript to the tdy_requests.js

--- app/assets/javascripts/tdy_requests.js
+++ app/assets/javascripts/tdy_requests.js
@@ -4,3 +4,21 @@
 //     });
 // });
 
+$(document).ready(function() {
+    $(".traveller").click(function(event) {
+        var name =this.nextSibling.data.trim();
+        if (this.checked) {
+            $('#sel1').append("<option value="+ this.value +" class='drivers'>" + name + "</option>");
+        } else {
+            $('.drivers[value="' + this.value + '"]').remove();
+        };
+    });
+    
+    $('#pomv_check').click(function() {
+        $('#pomv-details').css('visibility', $('#pomv_check').prop('checked')? 'visible' : 'hidden');
+    });
+    
+    $( "#tdy_request_destination" ).autocomplete({
+      source:  $('#tdy_request_destination').data('autocomplete-source')
+    });
+});
