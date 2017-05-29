b5c3c8faea53c7006a64a73218e315db2985e838
Joaquin Morcate <jmorcate@gmail.com>
2017-05-29 19:25:06 +0000

    Added the claims controller and the new claims view. Added the link to the tdy request patial for new claim

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,13 +1,27 @@
 class ClaimsController < ApplicationController
   
   def new
+    @claim = Claim.new
+    
+    # The following two lines are equivalent. 
+    #@claim.tdy_request = TdyRequest.find(params[:request])
+    @claim.tdy_request_id = params[:request]
   end
 
   def create
+    @claim = Claim.new(claim_params)
+
+    if @claim.save
+      flash[:notice] = "The <b>#{ @claim.id }</b> claim has been saved successfully."
+      redirect_to(claims_path, :notice => "The <b>#{ @clain.id }</b> claim has been saved successfully.")
+    else
+      render(:new, :error => @claim.errors)
+    end
+    
   end
   
   private 
-    def claims_params
+    def claim_params
       params.require(:claim).permit(:number_days_hotel, :breakfast, :meals_provided_by_location, :meals_provided, 
         legs_attributes: [:mode_transport,:depart, :depart_time, :destination, :destination_time] )
     end
